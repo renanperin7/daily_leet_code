d5eba041e54029140ab30d1fa9ddb6480f5e1441
Renan <renanperin27@hotmail.com>
2025-04-05 11:11:02 -0300

    LeetCode 05/04

---
+++ src/LongestBalancedSubstring.kt
@@ -0,0 +1,27 @@
+class LongestBalancedSubstring {
+    fun findTheLongestBalancedSubstring(s: String): Int {
+        var maxLen = 0
+        var i = 0
+        val n = s.length
+
+        while (i < n) {
+            var zeros = 0
+            var ones = 0
+
+            while (i < n && s[i] == '0') {
+                zeros++
+                i++
+            }
+
+            while (i < n && s[i] == '1') {
+                ones++
+                i++
+            }
+
+            val currentLen = minOf(zeros, ones) * 2
+            maxLen = maxOf(maxLen, currentLen)
+        }
+
+        return maxLen
+    }
+}
---
+++ src/TwoSum.kt
@@ -0,0 +1,23 @@
+class TwoSum {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val hashmap = HashMap<Int, Int>()
+
+        for ((i, num) in nums.withIndex()) {
+            val complement = target - num
+
+            if (hashmap.containsKey(complement)) {
+                return intArrayOf(hashmap[complement]!!, i)
+            }
+
+            hashmap[num] = i
+        }
+
+        return intArrayOf()
+    }
+
+    /*
+    * Input: nums = [2,7,11,15], target = 9
+    * Output: [0,1]
+    * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+    */
+}
