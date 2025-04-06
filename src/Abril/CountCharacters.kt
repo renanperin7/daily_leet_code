0ca1eb186c8fb293e88e002c0f10a168a9005e9e
Renan <renanperin27@hotmail.com>
2025-04-06 10:45:19 -0300

    LeetCode 06/04

---
+++ src/Abril/CountCharacters.kt
@@ -0,0 +1,34 @@
+package Abril
+
+class CountCharacters {
+    fun countCharacters(words: Array<String>, chars: String): Int {
+        val charsHashMap = HashMap<Char, Int>()
+        var totalLength = 0
+
+        chars.forEach { char ->
+            charsHashMap[char] = charsHashMap.getOrDefault(char, 0) + 1
+        }
+
+        words.forEach { word ->
+            val wordHashMap = HashMap<Char, Int>()
+            var isValid = true
+
+            word.forEach { letra ->
+                wordHashMap[letra] = wordHashMap.getOrDefault(letra, 0) + 1
+            }
+
+            for ((letra, count) in wordHashMap) {
+                if (charsHashMap.getOrDefault(letra, 0) < count) {
+                    isValid = false
+                    break
+                }
+            }
+
+            if (isValid) {
+                totalLength += word.length
+            }
+        }
+
+        return totalLength
+    }
+}
--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,4 @@
+import Abril.CountCharacters
 import Abril.IsPalindrome
 import Abril.LongestBalancedSubstring
 import Abril.TwoSum
@@ -7,10 +8,13 @@ fun main() {
     val longestBalancedSubstring = LongestBalancedSubstring()
     val twoSum = TwoSum()
     val isPalindrome = IsPalindrome()
+    val countCharacters = CountCharacters()
 
     longestBalancedSubstring.findTheLongestBalancedSubstring("000001111")
 
     twoSum.twoSum(intArrayOf(2, 7, 11, 15), 9)
 
     isPalindrome.isPalindrome(121)
+
+    countCharacters.countCharacters(arrayOf("cat", "swap", "dereame"), "cataa")
 }
